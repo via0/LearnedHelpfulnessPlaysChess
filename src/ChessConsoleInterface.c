af61db5dbf9a4992f33510d0d934d423fdd39ec7
Jack Greer <jvpng1@gmail.com>
2026-01-23 21:40:35 -0700

    ChessConsoleInterface initiated

---
+++ src/ChessConsoleInterface.c
@@ -0,0 +1,2 @@
+
+#include "ChessConsoleInterface.h"
---
+++ test/test_ChessConsoleInterface.c
@@ -0,0 +1,21 @@
+
+#ifdef TEST
+
+#include "unity.h"
+
+#include "ChessConsoleInterface.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_ChessConsoleInterface_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement ChessConsoleInterface");
+}
+
+#endif // TEST
