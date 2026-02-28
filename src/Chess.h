5360800e71e42bb6389a38609df2c35bb3903ea2
Jack Greer <jvpng1@gmail.com>
2026-02-28 10:51:39 -0700

    add Chess module

---
+++ src/Chess.h
@@ -0,0 +1,10 @@
+
+#ifndef CHESS_H
+#define CHESS_H
+
+typedef enum {
+    CHESS_STATE_SELECTING_START_SQUARE = 0
+} ChessStateT;
+
+
+#endif // CHESS_H
---
+++ test/test_Chess.c
@@ -0,0 +1,21 @@
+
+#ifdef TEST
+
+#include "unity.h"
+
+#include "Chess.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_Chess_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement Chess");
+}
+
+#endif // TEST
